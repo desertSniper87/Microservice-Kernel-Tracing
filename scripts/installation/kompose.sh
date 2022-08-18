8168fbe0bad2679c78e97b0818eb659da5914b08
desertsniper87 <torshobuet@gmail.com>
2022-08-18 21:36:38 +0600

    kompose

---
+++ scripts/installation/kompose.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo 'Setting up compose'
+
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
+cd /usr/bin
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.0/kompose-linux-amd64 -o kompose
+chmod +x kompose
+
+
